37be8d5671598e79575e5587cc8d1f20c9f4c422
Bonaventure Ogeto <bonaogeto1@gmail.com>
2022-09-11 19:12:38 +0300

    team/organization models

--- flashteams/accounts/models.py
+++ flashteams/accounts/models.py
@@ -1,2 +1,16 @@
 from django.db import models
+from utils.models import BaseAbstractModel
+from utils.managers import CustomQuerySet
 
+
+class Team(BaseAbstractModel):
+    "models for creating a team or organization"
+    name = models.CharField(max_length=25)
+    description = models.TextField(max_length=255)
+
+    active_objects = CustomQuerySet.as_manager()
+    objects = models.Manager()
+
+    def __str__(self):
+        return f'{self.name}'
+    
