3e0dba7e507bf99cd8f4d86818b30adad00dc03a
Bonaventure Ogeto <bonaogeto1@gmail.com>
2022-09-15 01:43:06 +0300

    new user registration api view

--- flashteams/authentication/views.py
+++ flashteams/authentication/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.generics import GenericAPIView, CreateAPIView
+from authentication.serializers import RegistrationSerializer, LoginSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class RegistrationAPIView(GenericAPIView):
+    """Register new users."""
+
+    serializer_class = RegistrationSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        user_data = serializer.data
+
+        response = {
+            "data": {
+                "user": user_data,
+                "message": "user created successfully"
+            }
+        }
+
+        return Response(response, status=status.HTTP_201_CREATED)
+
+
+
