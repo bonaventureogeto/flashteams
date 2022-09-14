2f9668b9575a0c490381be21d239f4cbfea58430
Bonaventure Ogeto <bonaogeto1@gmail.com>
2022-09-15 01:39:23 +0300

    #update

--- flashteams/authentication/models.py
+++ flashteams/authentication/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import (AbstractUser, BaseUserManager)
-from django.contrib.postgres.fields import JSONField
+from django.db.models import JSONField
 from django.core.serializers.json import DjangoJSONEncoder
 from datetime import datetime, timedelta
 import jwt
